e7f8aa946c4016229d0f70c592a58ac0504a7dc8
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-03-14 23:03:10 +0530

    Add 1 file

---
+++ EmpWageOops.java
@@ -0,0 +1,38 @@
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class EmpWageOops {
+
+	public static void main(String[] args) {
+
+		Attendance attendance = new Attendance();
+		attendance.EmployeeCheck();
+	}
+}
+
+class Attendance {
+	final int IS_PRESENT = 1;
+	final int WAGE_PER_HOUR = 20;
+	final int IS_FULL_TIME = 8;
+	int dailyWage = 0;
+
+	int GetEmployeePresent(int a) {
+		Random r = new Random();
+		int empCheck = r.nextInt(a);
+		return empCheck;
+	}
+
+	void EmployeeCheck() {
+		int empCheck = GetEmployeePresent(2);
+		if (empCheck == IS_PRESENT) {
+			System.out.println("Employee is present");
+			dailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
+
+		} else {
+			System.out.println("Employee is Absent");
+			dailyWage = dailyWage;
+		}
+		System.out.println("Daily Wage : " + dailyWage);
+	}
+}
