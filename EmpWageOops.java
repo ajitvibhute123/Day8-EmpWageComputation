6fe7477278fbd8341998dcea91e56cb40edfb810
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-03-14 23:24:16 +0530

    Add 1 file

--- EmpWageOops.java
+++ EmpWageOops.java
@@ -1,38 +1,47 @@
 package com.bridgelabz;
-
 import java.util.Random;
 
-public class EmpWageOops {
+    public class EmpWageOops {
 
-	public static void main(String[] args) {
-
-		Attendance attendance = new Attendance();
-		attendance.EmployeeCheck();
-	}
+        public static void main(String[] args) {
+            Attendance attendance = new Attendance();
+            attendance.EmployeeCheck();
+    }
 }
+    class Attendance{
+        static final int IS_PRESENT = 1;
+        static final int IS_ABSENT = 0;
+        static final int WAGE_PER_HOUR = 20;
+        static final int IS_FULL_TIME = 8;
+        static final int IS_PART_TIME = 4;
+        int dailyWage = 0;
 
-class Attendance {
-	final int IS_PRESENT = 1;
-	final int WAGE_PER_HOUR = 20;
-	final int IS_FULL_TIME = 8;
-	int dailyWage = 0;
+    int GetEmployeePresent(int a) {
+        Random r = new Random();
+        int empCheck = r.nextInt(a);
+        return empCheck;
+    }
 
-	int GetEmployeePresent(int a) {
-		Random r = new Random();
-		int empCheck = r.nextInt(a);
-		return empCheck;
-	}
+    void EmployeeCheck() {
+        int empCheck = GetEmployeePresent(3);
+        switch (empCheck) {
+            case IS_PRESENT: {
+                System.out.println("Employee is present");
+                dailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
+                break;
 
-	void EmployeeCheck() {
-		int empCheck = GetEmployeePresent(2);
-		if (empCheck == IS_PRESENT) {
-			System.out.println("Employee is present");
-			dailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
-
-		} else {
-			System.out.println("Employee is Absent");
-			dailyWage = dailyWage;
-		}
-		System.out.println("Daily Wage : " + dailyWage);
-	}
+            }
+            case IS_ABSENT: {
+                System.out.println("Employee is Absent");
+               
+                break;
+            }
+            default: {
+                System.out.println("Employee is Part time");
+                dailyWage = WAGE_PER_HOUR * IS_PART_TIME;
+                break;
+            }
+        }
+        System.out.println("Daily Wage : " + dailyWage);
+    }
 }
